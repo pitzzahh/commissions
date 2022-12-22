72900bacae845d301e9a11a1df581f142b610ea4
pitzzahh <pitzzahh@gmail.com>
2022-12-22 18:44:33 +0800

    quick update

--- src/crms/CriminalRecordManagementSystem.java
+++ src/crms/CriminalRecordManagementSystem.java
@@ -1,4 +1,4 @@
-package criminal;
+package crms;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CriminalRecordManagementSystem {
@@ -21,10 +22,10 @@ public class CriminalRecordManagementSystem {
         do {
             System.out.println("Welcome to the Criminal Record Management System");
             System.out.println("Please select an option:");
-            System.out.println("1. Add a new criminal record");
-            System.out.println("2. View existing criminal record");
-            System.out.println("3. Update a criminal record");
-            System.out.println("4. Delete a criminal record");
+            System.out.println("1. Add a new crms record");
+            System.out.println("2. View existing crms record");
+            System.out.println("3. Update a crms record");
+            System.out.println("4. Delete a crms record");
             System.out.println("5. Exit the program");
             System.out.print(">>: ");
 
@@ -54,9 +55,9 @@ public class CriminalRecordManagementSystem {
 
     }
 
-    public static void addCriminalRecord() throws IOException, InterruptedException {
+    public static void addCriminalRecord() throws InterruptedException {
 
-        System.out.println("Please enter the following information for the new criminal record:");
+        System.out.println("Please enter the following information for the new crms record:");
         System.out.print("Name: ");
         String name = input.nextLine().trim();
 
@@ -107,19 +108,19 @@ public class CriminalRecordManagementSystem {
 
         writeToATextFile(newCriminal.toString(), file, true);
 
-        System.out.println("Successfully added new criminal record for " + newCriminal.getName());
+        System.out.println("Successfully added new crms record for " + newCriminal.getName());
     }
 
     public static void viewCriminalRecord() {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Please enter the name of the criminal whose record you would like to view: ");
+        System.out.print("Please enter the name of the crms whose record you would like to view: ");
         String name = input.nextLine();
 
         Criminal criminal = findCriminal(name);
 
         if (criminal == null) {
-            System.out.println("No criminal record found for " + name);
+            System.out.println("No crms record found for " + name);
         } else {
             System.out.println(criminal);
         }
@@ -127,13 +128,13 @@ public class CriminalRecordManagementSystem {
 
     public static void updateCriminalRecord() {
 
-        System.out.print("Please enter the name of the criminal whose record you would like to update: ");
+        System.out.print("Please enter the name of the crms whose record you would like to update: ");
         String name = input.nextLine();
 
         Criminal criminal = findCriminal(name);
 
         if (criminal == null) {
-            System.out.println("No criminal record found for " + name);
+            System.out.println("No crms record found for " + name);
         } else {
             System.out.println("What would you like to update?");
             System.out.println("1. Name");
@@ -187,7 +188,7 @@ public class CriminalRecordManagementSystem {
                     criminal.setLengthOfImprisonment(input.nextLine().trim());
                     break;
                 case "9":
-                    System.out.print("Is the criminal still detained?: ");
+                    System.out.print("Is the crms still detained?: ");
                     criminal.setDetained(input.nextLine().trim().equalsIgnoreCase("T"));
                     break;
                 case "10":
@@ -196,32 +197,32 @@ public class CriminalRecordManagementSystem {
                     System.out.println("Invalid selection. Please try again.");
                     break;
             }
-            if (!choice.equals("10")) System.out.println("Successfully updated criminal record for " + name);
+            if (!choice.equals("10")) System.out.println("Successfully updated crms record for " + name);
         }
     }
 
-    public static void deleteCriminalRecord() throws IOException, InterruptedException {
+    public static void deleteCriminalRecord() throws InterruptedException {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Please enter the name of the criminal whose record you would like to delete: ");
+        System.out.print("Please enter the name of the crms whose record you would like to delete: ");
         String name = input.nextLine();
 
         Criminal criminal = findCriminal(name);
 
         if (criminal == null) {
-            System.out.println("No criminal record found for " + name);
+            System.out.println("No crms record found for " + name);
         } else {
             deleteCriminal(name);
             writeToATextFile("", file, false); // remove lahat ng nasa file
             for (Criminal value : criminals) {
                 writeToATextFile(value.toString(), file, true);
             }
-            System.out.println("Successfully deleted criminal record for " + name);
+            System.out.println("Successfully deleted crms record for " + name);
         }
     }
 
     public static Criminal findCriminal(String name) {
-        // Method to search for a criminal record by name and return the Criminal object if found
+        // Method to search for a crms record by name and return the Criminal object if found
         // Return null if no record is found
 
         for (Criminal criminal : criminals) {
@@ -236,7 +237,7 @@ public class CriminalRecordManagementSystem {
         criminals.removeIf(criminal -> criminal.getName().equals(name));
     }
 
-    public static void writeToATextFile(String whatToWrite, File fileToWrite, boolean writeToBottom) throws IOException, InterruptedException {
+    public static void writeToATextFile(String whatToWrite, File fileToWrite, boolean writeToBottom) throws InterruptedException {
         Thread thread = new Thread(
                 () -> {
                     try {
@@ -252,4 +253,139 @@ public class CriminalRecordManagementSystem {
         thread.join();
         thread.start();
     }
+}
+
+class Criminal {
+
+    private String name;
+    private String age;
+    private String gender;
+    private String address;
+    private String crimeCommitted;
+    private String policeId;
+    private String policeRank;
+    private String detainedStation;
+    private String lengthOfImprisonment;
+    private boolean isDetained;
+
+    public Criminal(String name, String age, String gender, String address, String crimeCommitted, String policeId, String policeRank, String detainedStation, String lengthOfImprisonment, boolean isDetained) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+        this.crimeCommitted = crimeCommitted;
+        this.policeId = policeId;
+        this.policeRank = policeRank;
+        this.detainedStation = detainedStation;
+        this.lengthOfImprisonment = lengthOfImprisonment;
+        this.isDetained = isDetained;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCrimeCommitted() {
+        return crimeCommitted;
+    }
+
+    public void setCrimeCommitted(String crimeCommitted) {
+        this.crimeCommitted = crimeCommitted;
+    }
+
+    public String getPoliceId() {
+        return policeId;
+    }
+
+    public void setPoliceId(String policeId) {
+        this.policeId = policeId;
+    }
+
+    public String getPoliceRank() {
+        return policeRank;
+    }
+
+    public void setPoliceRank(String policeRank) {
+        this.policeRank = policeRank;
+    }
+
+    public String getDetainedStation() {
+        return detainedStation;
+    }
+
+    public void setDetainedStation(String detainedStation) {
+        this.detainedStation = detainedStation;
+    }
+
+    public String getLengthOfImprisonment() {
+        return lengthOfImprisonment;
+    }
+
+    public void setLengthOfImprisonment(String lengthOfImprisonment) {
+        this.lengthOfImprisonment = lengthOfImprisonment;
+    }
+
+    public boolean isDetained() {
+        return isDetained;
+    }
+
+    public void setDetained(boolean detained) {
+        this.isDetained = detained;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Criminal)) return false;
+        Criminal criminal = (Criminal) o;
+        return isDetained() == criminal.isDetained() && getName().equals(criminal.getName()) && getAge().equals(criminal.getAge()) && getGender().equals(criminal.getGender()) && getAddress().equals(criminal.getAddress()) && getCrimeCommitted().equals(criminal.getCrimeCommitted()) && getPoliceId().equals(criminal.getPoliceId()) && getPoliceRank().equals(criminal.getPoliceRank()) && getDetainedStation().equals(criminal.getDetainedStation()) && getLengthOfImprisonment().equals(criminal.getLengthOfImprisonment());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge(), getGender(), getAddress(), getCrimeCommitted(), getPoliceId(), getPoliceRank(), getDetainedStation(), getLengthOfImprisonment(), isDetained());
+    }
+
+    @Override
+    public String toString() {
+        return "Name                 : " + getName() + "\n" +
+                "Age                  : " + getAge() + "\n" +
+                "Gender               : " + getGender() + "\n" +
+                "Address              : " + getAddress() + "\n" +
+                "Crime Committed      : " + getCrimeCommitted() + "\n" +
+                "Police ID            : " + getPoliceId() + "\n" +
+                "Police Rank          : " + getPoliceRank() + "\n" +
+                "Detained Station     : " + getDetainedStation() + "\n" +
+                "Years of imprisonment: " + getLengthOfImprisonment() + "\n" +
+                "Is detained          : " + isDetained() + "\n";
+    }
+
 }
